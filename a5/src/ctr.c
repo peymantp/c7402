5179b2dce7795dc0af468b9dd608f941c906bb55
dimitry rakhlei <dimitryrakhlei@gmail.com>
2019-03-21 15:45:07 -0700

    adding message for ctr_entropy

--- a5/src/ctr.c
+++ a5/src/ctr.c
@@ -1,43 +1,51 @@
 #include "ctr.h"
 
-void encrypt_ctr(FILE *infile, FILE *outfile, uint32_t rounds, uint32_t keys[]) {
+void encrypt_ctr(FILE *infile, FILE *outfile, uint32_t rounds,
+                 uint32_t keys[]) {
     srand(keys[0]); // make the nonce based on supplied key
     uint64_t nonce = rand() % (1024);
     uint32_t left, right;
     size_t ret;
     uint64_t sblock;
-    while(!feof(infile)) {
+    while (!feof(infile)) {
         // read data
         memset(&sblock, 0, sizeof(sblock));
         ret = fread(&sblock, 1, sizeof(sblock), infile);
-        if(!ret) break;
+        if (!ret)
+            break;
 
         // block cipher the nonce
-        left = (nonce>>32) & 0xFFFFFFFF;
+        left = (nonce >> 32) & 0xFFFFFFFF;
         right = nonce & 0xFFFFFFFF;
-        sblock ^= encrypt(left, right, ROUNDS, keys); // xor the block cipher output with the data block
+        sblock ^=
+            encrypt(left, right, ROUNDS,
+                    keys); // xor the block cipher output with the data block
         ret = fwrite(&sblock, 1, sizeof(sblock), outfile); // write data out
-        nonce++; // counter
+        nonce++;                                           // counter
     }
 }
 
-void decrypt_ctr(FILE *infile, FILE *outfile, uint32_t rounds, uint32_t keys[]) {
+void decrypt_ctr(FILE *infile, FILE *outfile, uint32_t rounds,
+                 uint32_t keys[]) {
     srand(keys[0]); // make the nonce based on supplied key
     uint64_t nonce = rand() % (1024);
     uint32_t left, right;
     size_t ret;
     uint64_t sblock;
-    while(!feof(infile)) {
+    while (!feof(infile)) {
         // read data
         memset(&sblock, 0, sizeof(sblock));
         ret = fread(&sblock, 1, sizeof(sblock), infile);
-        if(!ret) break;
+        if (!ret)
+            break;
 
         // block cipher the nonce
-        left = (nonce>>32) & 0xFFFFFFFF;
+        left = (nonce >> 32) & 0xFFFFFFFF;
         right = nonce & 0xFFFFFFFF;
-        sblock ^= encrypt(left, right, ROUNDS, keys); // xor the block cipher output with the data block
+        sblock ^=
+            encrypt(left, right, ROUNDS,
+                    keys); // xor the block cipher output with the data block
         ret = fwrite(&sblock, 1, sizeof(sblock), outfile); // write data out
-        nonce++; // counter
+        nonce++;                                           // counter
     }
 }
